24c58e2ddc3d0a42c80440c38e20449c552d18ab
shenqiang <13032119695@163.com>
2019-02-14 17:08:41 +0800

    plus new page

--- pages/PurchaseOrderInquiry/PurchaseOrderInquiry.vue
+++ pages/PurchaseOrderInquiry/PurchaseOrderInquiry.vue
@@ -1,8 +1,55 @@
 <template>
-	<div></div>
+	<view>
+	<PurchaseOrderInquiryHead v-if="PurchaseOrderInquiryHead_show"/>
+	<PurchaseOrderInquirybodyA v-if="PurchaseOrderInquirybodyA_show"/>
+	</view>
+	
 </template>
 
 <script>
+	import {} from 'vuex'
+	import PurchaseOrderInquiryHead from "../components/PurchaseOrderInquiryHead/PurchaseOrderInquiryHead.vue"
+	import PurchaseOrderInquirybodyA from "../components/PurchaseOrderInquirybodyA/PurchaseOrderInquirybodyA.vue"
+	import PurchaseOrderInquirybodyB from "../components/PurchaseOrderInquirybodyB/PurchaseOrderInquirybodyB.vue"
+	export default {
+		components: {
+			PurchaseOrderInquiryHead,
+			PurchaseOrderInquirybodyA,
+			PurchaseOrderInquirybodyB
+		},
+	
+		data() {
+			return {
+				PurchaseOrderInquiryHead_show:false,
+				PurchaseOrderInquirybodyA_show:true
+			};
+		},
+		onNavigationBarButtonTap() {
+			
+		},
+		computed: {
+			i18n() {
+				return this.$t('homepage')
+			}
+		},
+	
+		methods: {
+			scanCode() {
+				uni.scanCode({
+					success: function(res) {
+						console.log('条码类型：' + res.scanType);
+						console.log('条码内容：' + res.result);
+					}
+				});
+			},
+			success() {
+				this.ifshowmodal = !this.ifshowmodal
+			},
+		},
+		onLoad() {
+			
+		}
+	}
 </script>
 
 <style>
